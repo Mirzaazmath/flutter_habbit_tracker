21366d71ae274a5d332c04fb065905558f76cb57
Mirzaazmath <azmath9603399270@gmail.com>
2025-07-11 16:42:59 +0530

    added the switch to change the theme in home screen drawer

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,9 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:habbit_tracker/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        child: Center(
+          child: CupertinoSwitch(
+            value: Provider.of<ThemeProvider>(context).isDarkMode,
+            onChanged: (value) {
+              Provider.of<ThemeProvider>(context,listen: false).toggleTheme();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
