a7d72321774a6c39ea8ceb34d7d29f742de74e97
Mirzaazmath <azmath9603399270@gmail.com>
2025-07-11 16:37:09 +0530

    implemented theme provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:habbit_tracker/themes/theme_provider.dart';
 import 'package:habbit_tracker/ui/home_screen.dart';
-void main(){
-  runApp(MyApp());
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+        home: HomeScreen(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
